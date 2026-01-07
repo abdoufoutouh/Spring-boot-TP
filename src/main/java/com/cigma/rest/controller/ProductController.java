c066ef413816acf18937e77a33b52817a1852b2a
Abderrahmane <foutouhabderrahman8@gmail.com>
2026-01-07 21:02:57 +0100

    Implémentation CRUD ProductController :)

---
+++ src/main/java/com/cigma/rest/controller/ProductController.java
@@ -0,0 +1,44 @@
+package com.cigma.rest.controller;
+import com.cigma.rest.modes.Product;
+import com.cigma.rest.service.IProductService;
+import com.cigma.rest.service.ProductServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    @Autowired
+    private IProductService productService;
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+       return productService.getAllProducts();
+    }
+
+    @GetMapping("/products/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        Product product = productService.getProductById(id).orElse(null);
+        return product;}
+
+    @PostMapping("/products")
+    public Product createProduct(@RequestBody Product product) {
+        productService.createProduct(product);
+        return product;}
+
+    @PutMapping("/products/{id}")
+    public void updateProduct(@PathVariable Long id
+            , @RequestBody Product product) {
+        productService.updateProduct(id, product);}
+
+    @DeleteMapping("/products/{id}")
+    public void deleteProduct(@PathVariable Long id ) {
+        productService.deleteProduct(id);
+    }
+
+
+
+}
--- src/main/java/com/cigma/rest/modes/Product.java
+++ src/main/java/com/cigma/rest/modes/Product.java
@@ -5,14 +5,14 @@ public class Product {
   private String name;
 
 
-  public Product(String name , Long id) {
+    public Product(String name , Long id) {
     this.name = name;
     this.id = id;}
 
     public Long getId() {return id;}
     public void setId(Long id) {this.id = id;}
-  public String getName() {return name;}
-  public void setName(String name) {this.name = name;}
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
 
 
 }
